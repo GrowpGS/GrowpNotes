acd13341871fa33bc11fcc61e3a0818cd634b5c8
Thales Gregorio <137453849+GrowpGS@users.noreply.github.com>
2025-01-17 13:24:15 -0300

    Init commit

---
+++ growp-studios/src/main.jsx
@@ -0,0 +1,18 @@
+// CSS
+import "./index.css";
+
+// React
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// Components
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
